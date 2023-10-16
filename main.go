7194bda8fc708289daf1e9c3a81be10aab1804c6
Kensei Nakada <handbomusic@gmail.com>
2023-10-16 15:44:43 +0900

    enable Leader Election

--- main.go
+++ main.go
@@ -113,7 +113,7 @@ func main() {
 		// the manager stops, so would be fine to enable this option. However,
 		// if you are doing or is intended to do any operation such as perform cleanups
 		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
+		LeaderElectionReleaseOnCancel: true,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
