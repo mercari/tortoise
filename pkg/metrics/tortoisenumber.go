9e290e525b20610bef553748d27b9bb25e133314
Kensei Nakada <handbomusic@gmail.com>
2024-01-25 19:04:11 +0900

    fix: reset tortoise_number metric correctly when tortoise changes mode

--- pkg/metrics/tortoisenumber.go
+++ pkg/metrics/tortoisenumber.go
@@ -9,14 +9,30 @@ func RecordTortoise(t *v1beta3.Tortoise, deleted bool) {
 	if deleted {
 		value = 0
 	}
-	TortoiseNumber.WithLabelValues(
-		t.Name,
-		t.Namespace,
-		t.Spec.TargetRefs.ScaleTargetRef.Name,
-		t.Spec.TargetRefs.ScaleTargetRef.Kind,
-		string(t.Spec.UpdateMode),
-		string(t.Status.TortoisePhase),
-	).Set(value)
+
+	for _, um := range []v1beta3.UpdateMode{v1beta3.UpdateModeOff, v1beta3.UpdateModeAuto, v1beta3.UpdateModeEmergency} {
+		if t.Spec.UpdateMode != um {
+			// Set 0 to reset.
+			TortoiseNumber.WithLabelValues(
+				t.Name,
+				t.Namespace,
+				t.Spec.TargetRefs.ScaleTargetRef.Name,
+				t.Spec.TargetRefs.ScaleTargetRef.Kind,
+				string(um),
+				string(t.Status.TortoisePhase),
+			).Set(0.0)
+			continue
+		}
+
+		TortoiseNumber.WithLabelValues(
+			t.Name,
+			t.Namespace,
+			t.Spec.TargetRefs.ScaleTargetRef.Name,
+			t.Spec.TargetRefs.ScaleTargetRef.Kind,
+			string(t.Spec.UpdateMode),
+			string(t.Status.TortoisePhase),
+		).Set(value)
+	}
 }
 
 func ShouldRerecordTortoise(old, new *v1beta3.Tortoise) bool {
